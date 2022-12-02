4dbae76894201eb9df09a36bcbbb9ab05bf75bec
Alexandra Muresan <alexandraixy@gmail.com>
2022-12-02 12:15:16 +0200

    competed the task: created RecoverPasswordWrapper component with bg image and props.children

---
+++ src/components/shared/recover-password-wrapper/RecoverPasswordWrapper.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { styled, Theme, CSSObject } from "@mui/material/styles";
+import { Box } from "@mui/material";
+import recoverBackground from "./images/recoverPasswordBg.png";
+
+const RecoverPasswordWrapper = (props) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100%",
+        background: `#fff url(${recoverBackground}) no-repeat bottom center`,
+        backgroundSize: "cover",
+      }}
+    >
+      {props.children}
+    </Box>
+  );
+};
+
+export default RecoverPasswordWrapper;
