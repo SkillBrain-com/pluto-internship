f247f0e8f7b2cd6d6fb704baee19f605ec2d877c
deiu25 <d3iu25@gmail.com>
2022-12-03 21:55:27 +0200

    style done

--- src/components/shared/alert/Alert.jsx
+++ src/components/shared/alert/Alert.jsx
@@ -1,10 +1,11 @@
-import { Alert, Box, Stack, AlertTitle } from "@mui/material";
+import { Box, Stack, AlertTitle } from "@mui/material";
 import DoneRoundedIcon from "@mui/icons-material/DoneRounded";
 import CheckCircleRoundedIcon from "@mui/icons-material/CheckCircleRounded";
 import ErrorOutlineRoundedIcon from "@mui/icons-material/ErrorOutlineRounded";
 import WarningAmberRoundedIcon from "@mui/icons-material/WarningAmberRounded";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
 import React from "react";
+import { styled } from "@mui/material/styles";
 
 const MuiAlert = () => {
   return (
@@ -18,48 +19,62 @@ const MuiAlert = () => {
       }}
     >
       <Stack spacing={2}>
-        <Alert
+        <Stack
           sx={{
             display: "flex",
             flexDirection: "row",
             justifyContent: "space-between",
-            alignItems: "flex-start",
+            alignItems: "center",
             width: 350,
             height: 81,
             background: "#F7002B",
             borderRadius: "12px",
           }}
-          variant="filled"
-          icon={
-            <WarningAmberRoundedIcon
+        >
+          <WarningAmberRoundedIcon
+            sx={{
+              width: 40,
+              height: 40,
+              background: "rgba(255, 255, 255, 0.15)",
+              color: "#FFFFFF",
+              display: "inline-flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "50%",
+              marginLeft: 2,
+              padding: 1,
+            }}
+          />
+          <Stack sx={{ marginRight: 12 }}>
+            <AlertTitle
               sx={{
-                background: "rgba(255, 255, 255, 0.15)",
+                fontWeight: 500,
+                fontSize: 18,
                 color: "#FFFFFF",
               }}
-            />
-          }
-          onClose={{}}
-        >
-          <AlertTitle
+            >
+              Error Message
+            </AlertTitle>
+            <AlertTitle
+              sx={{
+                fontWeight: 300,
+                fontSize: 12,
+                fontStyle: "italic",
+                color: "#FFFFFF",
+              }}
+            >
+              Some message
+            </AlertTitle>
+          </Stack>
+          <CloseOutlinedIcon
             sx={{
-              fontWeight: 500,
-              fontSize: 18,
               color: "#FFFFFF",
+              marginRight: 2,
+              marginBottom: 4,
             }}
-          >
-            Error Message
-          </AlertTitle>
-          <AlertTitle
-            sx={{
-              fontWeight: 300,
-              fontSize: 12,
-              fontStyle: "italic",
-              color: "#FFFFFF",
-            }}
-          >
-            Some message
-          </AlertTitle>
-        </Alert>
+          />
+        </Stack>
+
         <Stack
           sx={{
             display: "flex",
@@ -74,9 +89,16 @@ const MuiAlert = () => {
         >
           <ErrorOutlineRoundedIcon
             sx={{
+              width: 40,
+              height: 40,
               background: "#0000000D 5%",
               color: "#FFFFFF",
+              display: "inline-flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "50%",
               marginLeft: 2,
+              padding: 1,
             }}
           />
           <Stack sx={{ marginRight: 9 }}>
@@ -104,271 +126,348 @@ const MuiAlert = () => {
             sx={{
               marginRight: 2,
               marginBottom: 4,
+              color: "#16171D",
             }}
           />
         </Stack>
-        <Alert
+
+        <Stack
           sx={{
             display: "flex",
             flexDirection: "row",
             justifyContent: "space-between",
-            alignItems: "flex-start",
+            alignItems: "center",
             width: 350,
             height: 81,
             background: "#00C271",
             borderRadius: "12px",
           }}
-          variant="filled"
-          icon={
-            <WarningAmberRoundedIcon
+        >
+          <WarningAmberRoundedIcon
+            sx={{
+              width: 40,
+              height: 40,
+              background: "#FFFFFF26 15%",
+              color: "#FFFFFF",
+              display: "inline-flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "50%",
+              marginLeft: 2,
+              padding: 1,
+            }}
+          />
+          <Stack sx={{ marginRight: 9 }}>
+            <AlertTitle
               sx={{
-                background: "#FFFFFF26 15%",
+                fontWeight: 500,
+                fontSize: 18,
                 color: "#FFFFFF",
               }}
-            />
-          }
-          onClose={{}}
-        >
-          <AlertTitle
+            >
+              Success Message
+            </AlertTitle>
+            <AlertTitle
+              sx={{
+                fontWeight: 300,
+                fontSize: 12,
+                fontStyle: "italic",
+                color: "#FFFFFF",
+              }}
+            >
+              Some message
+            </AlertTitle>
+          </Stack>
+          <CloseOutlinedIcon
             sx={{
-              fontWeight: 500,
-              fontSize: 18,
+              marginRight: 2,
+              marginBottom: 4,
               color: "#FFFFFF",
             }}
-          >
-            Success Message
-          </AlertTitle>
-          <AlertTitle
-            sx={{
-              fontWeight: 300,
-              fontSize: 12,
-              fontStyle: "italic",
-              color: "#FFFFFF",
-            }}
-          >
-            Some message
-          </AlertTitle>
-        </Alert>
-        <Alert
+          />
+        </Stack>
+
+        <Stack
           sx={{
             display: "flex",
             flexDirection: "row",
             justifyContent: "space-between",
-            alignItems: "flex-start",
+            alignItems: "center",
             width: 350,
             height: 81,
             background: "#6684FF",
             borderRadius: "12px",
           }}
-          variant="filled"
-          icon={
-            <WarningAmberRoundedIcon
+        >
+          <WarningAmberRoundedIcon
+            sx={{
+              width: 40,
+              height: 40,
+              background: "#FFFFFF26 15%",
+              color: "#FFFFFF",
+              display: "inline-flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "50%",
+              marginLeft: 2,
+              padding: 1,
+            }}
+          />
+          <Stack sx={{ marginRight: 14 }}>
+            <AlertTitle
               sx={{
-                background: "#FFFFFF26 15%",
+                fontWeight: 500,
+                fontSize: 18,
                 color: "#FFFFFF",
               }}
-            />
-          }
-          onClose={{}}
-        >
-          <AlertTitle
+            >
+              Info Message
+            </AlertTitle>
+            <AlertTitle
+              sx={{
+                fontWeight: 300,
+                fontSize: 12,
+                fontStyle: "italic",
+                color: "#FFFFFF",
+              }}
+            >
+              Some message
+            </AlertTitle>
+          </Stack>
+          <CloseOutlinedIcon
             sx={{
-              fontWeight: 500,
-              fontSize: 18,
+              marginRight: 2,
+              marginBottom: 4,
               color: "#FFFFFF",
             }}
-          >
-            Info Message
-          </AlertTitle>
-          <AlertTitle
-            sx={{
-              fontWeight: 300,
-              fontSize: 12,
-              fontStyle: "italic",
-              color: "#FFFFFF",
-            }}
-          >
-            Some message
-          </AlertTitle>
-        </Alert>
+          />
+        </Stack>
       </Stack>
       <Stack spacing={2}>
-        <Alert
+        <Stack
           sx={{
             display: "flex",
             flexDirection: "row",
             justifyContent: "space-between",
-            alignItems: "flex-start",
+            alignItems: "center",
             width: 350,
             height: 81,
             border: 1.6,
             borderRadius: "12px",
             borderColor: "#F7002B",
           }}
-          variant="outlined"
-          severity="error"
-          icon={
-            <WarningAmberRoundedIcon
+        >
+          <WarningAmberRoundedIcon
+            sx={{
+              width: 40,
+              height: 40,
+              background: "#FB151A0F 6%",
+              color: "#B80020",
+              display: "inline-flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "50%",
+              marginLeft: 2,
+              padding: 1,
+            }}
+          />
+          <Stack sx={{ marginRight: 11 }}>
+            <AlertTitle
               sx={{
-                background: "#FB151A0F 6%",
+                fontWeight: 500,
+                fontSize: 18,
                 color: "#B80020",
               }}
-            />
-          }
-          onClose={{}}
-        >
-          <AlertTitle
+            >
+              Some message
+            </AlertTitle>
+            <AlertTitle
+              sx={{
+                fontWeight: 300,
+                fontSize: 12,
+                fontStyle: "italic",
+                color: "#B80020",
+              }}
+            >
+              Some message
+            </AlertTitle>
+          </Stack>
+          <CloseOutlinedIcon
             sx={{
-              fontWeight: 500,
-              fontSize: 18,
+              marginRight: 2,
+              marginBottom: 4,
               color: "#B80020",
             }}
-          >
-            Some Message
-          </AlertTitle>
-          <AlertTitle
-            sx={{
-              fontWeight: 300,
-              fontSize: 12,
-              fontStyle: "italic",
-              color: "#B80020",
-            }}
-          >
-            Some message
-          </AlertTitle>
-        </Alert>
-        <Alert
+          />
+        </Stack>
+        <Stack
           sx={{
             display: "flex",
             flexDirection: "row",
             justifyContent: "space-between",
-            alignItems: "flex-start",
+            alignItems: "center",
             width: 350,
             height: 81,
             border: 1.6,
             borderRadius: "12px",
             borderColor: "#FBBE37",
           }}
-          variant="outlined"
-          severity="warning"
-          icon={
-            <WarningAmberRoundedIcon
-              sx={{
-                background: "#FAAE0514 8%",
-                color: "#FAAE05",
-              }}
-            />
-          }
-          onClose={{}}
         >
-          <AlertTitle
+          <WarningAmberRoundedIcon
             sx={{
-              fontWeight: 500,
-              fontSize: 18,
+              width: 40,
+              height: 40,
+              background: "#FAAE0514 8%",
+              color: "#FAAE05",
+              display: "inline-flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "50%",
+              marginLeft: 2,
+              padding: 1,
+            }}
+          />
+          <Stack sx={{ marginRight: 11 }}>
+            <AlertTitle
+              sx={{
+                fontWeight: 500,
+                fontSize: 18,
+                color: "#FBBE37",
+              }}
+            >
+              Some message
+            </AlertTitle>
+            <AlertTitle
+              sx={{
+                fontWeight: 300,
+                fontSize: 12,
+                fontStyle: "italic",
+                color: "#FBBE37",
+              }}
+            >
+              Some message
+            </AlertTitle>
+          </Stack>
+          <CloseOutlinedIcon
+            sx={{
+              marginRight: 2,
+              marginBottom: 4,
               color: "#FBBE37",
             }}
-          >
-            Some Message
-          </AlertTitle>
-          <AlertTitle
-            sx={{
-              fontWeight: 300,
-              fontSize: 12,
-              fontStyle: "italic",
-              color: "#FBBE37",
-            }}
-          >
-            Some message
-          </AlertTitle>
-        </Alert>
-        <Alert
+          />
+        </Stack>
+        <Stack
           sx={{
             display: "flex",
             flexDirection: "row",
             justifyContent: "space-between",
-            alignItems: "flex-start",
+            alignItems: "center",
             width: 350,
             height: 81,
             border: 1.6,
             borderRadius: "12px",
             borderColor: "#00C271",
           }}
-          variant="outlined"
-          severity="success"
-          icon={
-            <DoneRoundedIcon
+        >
+          <DoneRoundedIcon
+            sx={{
+              width: 40,
+              height: 40,
+              background: "#E6FFF4",
+              color: "#00C271",
+              display: "inline-flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "50%",
+              marginLeft: 2,
+              padding: 1,
+            }}
+          />
+          <Stack sx={{ marginRight: 11 }}>
+            <AlertTitle
               sx={{
-                background: "#E6FFF4",
+                fontWeight: 500,
+                fontSize: 18,
                 color: "#00C271",
               }}
-            />
-          }
-          onClose={{}}
-        >
-          <AlertTitle
+            >
+              Some message
+            </AlertTitle>
+            <AlertTitle
+              sx={{
+                fontWeight: 300,
+                fontSize: 12,
+                fontStyle: "italic",
+                color: "#00C271",
+              }}
+            >
+              Some message
+            </AlertTitle>
+          </Stack>
+          <CloseOutlinedIcon
             sx={{
-              fontWeight: 500,
-              fontSize: 18,
+              marginRight: 2,
+              marginBottom: 4,
               color: "#00C271",
             }}
-          >
-            Some Message
-          </AlertTitle>
-          <AlertTitle
-            sx={{
-              fontWeight: 300,
-              fontSize: 12,
-              fontStyle: "italic",
-              color: "#00C271",
-            }}
-          >
-            Some message
-          </AlertTitle>
-        </Alert>
-        <Alert
+          />
+        </Stack>
+        <Stack
           sx={{
             display: "flex",
             flexDirection: "row",
             justifyContent: "space-between",
-            alignItems: "flex-start",
+            alignItems: "center",
             width: 350,
             height: 81,
             border: 1.6,
             borderRadius: "12px",
             borderColor: "#6684FF",
           }}
-          variant="outlined"
-          severity="info"
-          icon={
-            <CheckCircleRoundedIcon
+        >
+          <CheckCircleRoundedIcon
+            sx={{
+              width: 40,
+              height: 40,
+              background: "#EDF0FC",
+              color: "#6684FF",
+              display: "inline-flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "50%",
+              marginLeft: 2,
+              padding: 1,
+            }}
+          />
+          <Stack sx={{ marginRight: 11 }}>
+            <AlertTitle
               sx={{
-                background: "#EDF0FC",
+                fontWeight: 500,
+                fontSize: 18,
                 color: "#6684FF",
               }}
-            />
-          }
-          onClose={{}}
-        >
-          <AlertTitle
+            >
+              Some message
+            </AlertTitle>
+            <AlertTitle
+              sx={{
+                fontWeight: 300,
+                fontSize: 12,
+                fontStyle: "italic",
+                color: "#6684FF",
+              }}
+            >
+              Some message
+            </AlertTitle>
+          </Stack>
+          <CloseOutlinedIcon
             sx={{
-              fontWeight: 500,
-              fontSize: 18,
+              marginRight: 2,
+              marginBottom: 4,
               color: "#6684FF",
             }}
-          >
-            Some Message
-          </AlertTitle>
-          <AlertTitle
-            sx={{
-              fontWeight: 300,
-              fontSize: 12,
-              fontStyle: "italic",
-              color: "#6684FF",
-            }}
-          >
-            Some message
-          </AlertTitle>
-        </Alert>
+          />
+        </Stack>
       </Stack>
     </Box>
   );
