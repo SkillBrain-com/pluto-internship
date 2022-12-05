ca900e154af8e527cc26de920eec0152beedb6c1
fposea <fposea@gmail.com>
2022-12-05 21:37:02 +0200

    add register and login page

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import Test from "./pages/TestPage";
 import Tasks from "./pages/Tasks";
 import "./App.css";
 import AddMates from "./pages/AddMates";
+import { CreateAccount } from "./pages/CreateAccount";
+import { Login } from "./pages/Login";
 
-function App() {
+const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Tasks />} />
         <Route path="/test" element={<Test />} />
         <Route path="/addmates" element={<AddMates />} />
+        <Route path="/account" element={<CreateAccount />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/CreateAccount.jsx
@@ -0,0 +1,21 @@
+import { Stack } from "@mui/material";
+import { Media } from "../components/authentication/Media";
+import { AuthForm } from "../components/authentication/AuthForm";
+
+export const CreateAccount = () => {
+  return (
+    <Stack
+      spacing={3}
+      direction="row"
+      sx={{
+        width: "1440px",
+        height: "1024px",
+        padding: "20px",
+        border: "1px solid",
+      }}
+    >
+      <Media align="left" hasCreate={false} />
+      <AuthForm align="left" hasLogin={true} />
+    </Stack>
+  );
+};
