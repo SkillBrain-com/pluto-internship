dac22fc600d200a0b3fc00f84784f033132623fb
ToDy95 <todyimmortal@gmail.com>
2022-12-13 19:51:31 +0200

    fix fonts

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,5 @@
 import { createTheme } from "@mui/material/styles";
-import GelionRegular from "./assets/fonts/Gelion Regular.ttf";
-import GelionBold from "./assets/fonts/Gelion Bold.ttf";
-import GelionLight from "./assets/fonts/Gelion Light.ttf";
+
 
 const theme = createTheme({
   palette: {
@@ -66,7 +64,7 @@ const theme = createTheme({
     },
   },
   typography: {
-    fontFamily: "Gelion, Roboto, Arial",
+    fontFamily: "Gelion",
   },
   components: {
     MuiCssBaseline: {
@@ -76,7 +74,6 @@ const theme = createTheme({
               font-style: normal;
               font-display: swap;
               font-weight: 400;
-              src: local('Gelion'), local('Gelion-Regular'), url(${GelionRegular}) format('woff');
               unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
             },
             @font-face {
@@ -84,7 +81,6 @@ const theme = createTheme({
                 font-style: bold;
                 font-display: swap;
                 font-weight: 700;
-                src: local('Gelion'), local('Gelion-Bold'), url(${GelionBold}) format('woff');
                 unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
               },
               @font-face {
@@ -92,7 +88,6 @@ const theme = createTheme({
                 font-style: light;
                 font-display: swap;
                 font-weight: 200;
-                src: local('Gelion'), local('Gelion-Light'), url(${GelionLight}) format('woff');
                 unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
               }
           `,
