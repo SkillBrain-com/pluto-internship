1235d1cfe1358bfb8c1d92a73792d1c78cbf6b7c
ToDy95 <todyimmortal@gmail.com>
2022-12-21 01:46:10 +0200

    backend from heroku + toata logica de create/edite/delete/update + login functional fara redirect (de implementat) de restu va ocupati voi , have fun

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ const App = () => {
 
         <Route path="/avatars" element={<Avatars />} />
         {/* <Route path="/alerts" element={<Alerts />} /> */}
-        {/* <Route path="/inputs" element={<Inputs />} /> */}
+        <Route path="/inputs" element={<Inputs />} />
 
         <Route path="/taskdetails" element={<TaskDetailsCardPage />} />
       </Routes>
